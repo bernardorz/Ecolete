029c7ff7a42c1781aea3842faf093a2e35785cb4
bernardorz <bejusouh123@gmail.com>
2021-05-17 10:03:30 -0300

    fianlizando back end da aplicação

---
+++ server/read.md
@@ -0,0 +1,4 @@
+npm install express
+npm install typescript -D
+npm install @types/express -D
+npm install ts-node -D
---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import path from 'path'
+import routes from './routes'
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+
+
+
+app.listen(3080);
